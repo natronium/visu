e51df3a3f5cd896e3277c0afdfc9cc19c0dc8227
na <>
2025-12-06 14:42:02 -0500

    Factor out cpal stream & deque filling

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,20 @@
 use bevy::prelude::*;
-use core::time;
 use cpal::{
-    SampleFormat, StreamConfig,
+    SampleFormat, Stream, StreamConfig,
     traits::{DeviceTrait as _, HostTrait, StreamTrait as _},
 };
-use crossbeam_channel::{bounded, unbounded};
+use crossbeam_channel::{Receiver, bounded};
 use std::{
     collections::VecDeque,
     io::{Write, stdout},
-    thread,
 };
 
 const SAMPLE_RATE: u32 = 48000;
 pub const BUFFER_CAPACITY: usize = SAMPLE_RATE as usize / 10; // hold on to 0.1s of audio
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // non_bevy()?;
-    bevy();
+    non_bevy();
+    // bevy();
 
     Ok(())
 }
@@ -24,47 +22,21 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 struct AudioInputPlugin;
 
 #[derive(Resource)]
-struct AudioBuffer(VecDeque<f32>);
+struct AudioBuffer(VecDeque<f32>, Stream);
 
 impl Plugin for AudioInputPlugin {
     fn build(&self, app: &mut App) {
-        let host = cpal::host_from_id(cpal::HostId::Jack).unwrap();
-        let device = host.default_input_device().unwrap();
-        let config = device
-            .supported_input_configs()
-            .unwrap()
-            .find(|c| c.channels() == 1 && c.sample_format() == SampleFormat::F32)
-            .unwrap()
-            .try_with_sample_rate(cpal::SampleRate(SAMPLE_RATE))
-            .unwrap();
-        println!("{config:?}");
-        let config: StreamConfig = config.into();
-        let (tx, rx) = bounded::<f32>(SAMPLE_RATE as usize);
-        let stream = device
-            .build_input_stream(
-                &config,
-                move |data: &[f32], _info| {
-                    for sample in data {
-                        tx.send(*sample).ok();
-                    }
-                },
-                |_e| {},
-                None,
-            )
-            .unwrap();
-        stream.play().unwrap();
+        let (rx, stream) = create_audioinput_stream();
 
         let update_buffer = move |mut buf_res: ResMut<AudioBuffer>| {
-            let buf = &mut buf_res.0;
-            buf.extend(rx.try_iter());
-            if buf.len() > BUFFER_CAPACITY {
-                let excess = buf.len() - BUFFER_CAPACITY;
-                buf.drain(0..excess);
-            }
+            refill_buffer_from_stream(&rx, &mut buf_res.0);
         };
 
-        app.insert_resource(AudioBuffer(VecDeque::with_capacity(BUFFER_CAPACITY * 2)))
-            .add_systems(Update, update_buffer);
+        app.insert_resource(AudioBuffer(
+            VecDeque::with_capacity(BUFFER_CAPACITY * 2),
+            stream,
+        ))
+        .add_systems(Update, update_buffer);
     }
 }
 
@@ -80,38 +52,13 @@ fn bevy() {
         .run();
 }
 
-fn non_bevy() -> Result<(), Box<dyn std::error::Error + 'static>> {
-    let host = cpal::host_from_id(cpal::HostId::Jack)?;
-    let device = host.default_input_device().unwrap();
-    let config = device
-        .supported_input_configs()?
-        .find(|c| c.channels() == 1 && c.sample_format() == SampleFormat::F32)
-        .unwrap()
-        .try_with_sample_rate(cpal::SampleRate(SAMPLE_RATE))
-        .unwrap();
-    println!("{config:?}");
-    let config: StreamConfig = config.into();
-    let (tx, rx) = bounded::<f32>(SAMPLE_RATE as usize);
-    let stream = device.build_input_stream(
-        &config,
-        move |data: &[f32], _info| {
-            for sample in data {
-                tx.send(*sample).ok();
-            }
-        },
-        |_e| {},
-        None,
-    )?;
-    stream.play()?;
+fn non_bevy() {
+    let (rx, stream) = create_audioinput_stream();
     let mut buf = VecDeque::<f32>::with_capacity(BUFFER_CAPACITY * 2);
-    Ok(loop {
-        buf.extend(rx.try_iter());
-        if buf.len() > BUFFER_CAPACITY {
-            let excess = buf.len() - BUFFER_CAPACITY;
-            buf.drain(0..excess);
-        }
+    loop {
+        refill_buffer_from_stream(&rx, &mut buf);
         audiobar(&buf);
-    })
+    }
 }
 
 fn audiobar(buf: &VecDeque<f32>) {
@@ -121,4 +68,41 @@ fn audiobar(buf: &VecDeque<f32>) {
     print!("\r{clear}");
     print!("\r[{rms:06.3}]{bar}");
     stdout().flush().unwrap();
+}
+
+fn create_audioinput_stream() -> (Receiver<f32>, Stream) {
+    let host = cpal::host_from_id(cpal::HostId::Jack).unwrap();
+    let device = host.default_input_device().unwrap();
+    let config = device
+        .supported_input_configs()
+        .unwrap()
+        .find(|c| c.channels() == 1 && c.sample_format() == SampleFormat::F32)
+        .unwrap()
+        .try_with_sample_rate(cpal::SampleRate(SAMPLE_RATE))
+        .unwrap();
+    println!("{config:?}");
+    let config: StreamConfig = config.into();
+    let (tx, rx) = bounded::<f32>(SAMPLE_RATE as usize);
+    let stream = device
+        .build_input_stream(
+            &config,
+            move |data: &[f32], _info| {
+                for sample in data {
+                    tx.send(*sample).ok();
+                }
+            },
+            |_e| {},
+            None,
+        )
+        .unwrap();
+    stream.play().unwrap();
+    (rx, stream)
+}
+
+fn refill_buffer_from_stream(rx: &Receiver<f32>, buf: &mut VecDeque<f32>) {
+    buf.extend(rx.try_iter());
+    if buf.len() > BUFFER_CAPACITY {
+        let excess = buf.len() - BUFFER_CAPACITY;
+        buf.drain(0..excess);
+    }
 }
